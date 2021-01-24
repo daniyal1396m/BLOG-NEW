7e5ca6621b973fce30f17cc3cb2d3206a4f96633
DaniyalMotahhari <Daniyal1396m@gmail.com>
2021-01-24 09:13:48 +0330

    write Category migration

--- database/migrations/2021_01_23_155811_create_categories_table.php
+++ database/migrations/2021_01_23_155811_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->boolean('status');
+            $table->integer('level',null);
             $table->timestamps();
         });
     }
