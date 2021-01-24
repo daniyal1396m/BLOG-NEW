db3a7cd8d59a7d665cf7c8c3f43531fd7ca9231a
DaniyalMotahhari <Daniyal1396m@gmail.com>
2021-01-24 09:16:34 +0330

    write CallUs migration

--- database/migrations/2021_01_23_183452_create_calluses_table.php
+++ database/migrations/2021_01_23_183452_create_calluses_table.php
@@ -15,6 +15,12 @@ class CreateCallusesTable extends Migration
     {
         Schema::create('calluses', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->string('lastname',50);
+            $table->string('email',100);
+            $table->integer('phone',null);
+            $table->string('title',50);
+            $table->string('body',50);
             $table->timestamps();
         });
     }
